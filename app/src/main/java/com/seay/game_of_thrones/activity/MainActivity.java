195dd1bdf36fc4162c1b8bea3e42abfa7594b051
andrewseay <aseay@boomtownroi.com>
2019-11-04 12:11:17 -0500

    Initial commit

---
+++ app/src/main/java/com/seay/game_of_thrones/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.seay.game_of_thrones.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.seay.game_of_thrones.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
